4bb589cce97c20426a703aaf69f1ad9eb4eb4f4a
Rodrigo <rodrigomafrarios@gmail.com>
2021-06-29 09:22:50 -0300

    refactor: change parameter dynamodb account table name

--- infra/system/ssm-dynamodb-accounts.tf
+++ infra/system/ssm-dynamodb-accounts.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "dynamodb-accounts-table" {
-  name = "${var.env}-dynamodb-accounts-table"
-  type = "String"
+  name  = "${var.env}-accounts"
+  type  = "String"
   value = aws_dynamodb_table.accounts.name
 }
