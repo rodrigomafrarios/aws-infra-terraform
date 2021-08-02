9211ec1aecf7b6a2775c52ba89bb16bb93e8ac7c
Rodrigo Mafra <rodrigo.mafra@zenklub.com>
2021-08-02 09:47:10 -0300

    chore: add signup lambda

---
+++ services/account/account-signup-lambda.tf
@@ -0,0 +1,18 @@
+module "account_signup_lambda" {
+  source        = "../../lambda"
+  name          = "signup"
+  s3_bucket_name = "${terraform.workspace}-${var.account_id}-${var.project}-deployment"
+  s3_key        = "deploy/signup/latest.zip"
+  account_id    = var.account_id
+  project       = var.project
+  iam_role_arn  = aws_iam_role.signup_iam_role.arn
+  layers        = [aws_lambda_layer_version.latest.arn]
+  environment_variables = {
+    "DYNAMODB_TABLE_ACCOUNTS" : var.dynamodb_table_accounts_name,
+    "JWT_SECRET" : var.sample_jwt_secret
+  }
+  resource_depends_on = [
+    null_resource.lambda_deployment_bucket_arn_depends_on
+  ]
+  aws_api_gateway_rest_api_id  = aws_api_gateway_rest_api.account.id
+}
