b2f59466d23cc092cf632a654d4f732b72604b08
Rodrigo Mafra <rodrigo.mafra@zenklub.com>
2021-08-02 09:52:53 -0300

    chore: move s3 deploy bucket for account path

---
+++ services/account/s3-deployment-bucket.tf
@@ -0,0 +1,16 @@
+module "s3_deployment_account_bucket" {
+  source = "../../s3_bucket"
+  bucket_name = "${terraform.workspace}-${var.account_id}-${var.project}-deployment"
+}
+
+resource "aws_s3_bucket_object" "signup" {
+  bucket = module.s3_deployment_account_bucket.bucket_id
+  key = "deploy/signup/"
+  source = "/dev/null"
+}
+
+resource "aws_s3_bucket_object" "login" {
+  bucket = module.s3_deployment_account_bucket.bucket_id
+  key = "deploy/login/"
+  source = "/dev/null"
+}
--- services/system/lambda-deployment-bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "lambda_deployment_bucket" {
-  bucket = "${terraform.workspace}-${var.account_id}-lambda-deployment-bucket"
-  acl    = "private"
-}
